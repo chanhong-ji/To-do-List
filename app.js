e4b85ebbb3367ed1b9ef8415bf21aa3234697bb9
chanhong-ji <jichanhong24@gmail.com>
2021-07-27 17:07:00 +0900

    Loading username

---
+++ app.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("h1");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USER_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USER_KEY, username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  greeting.innerText += ` ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USER_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
