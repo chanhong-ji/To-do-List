f8c0bba4bfb97ed86cf57004fea3ccfbe98eb990
chanhong-ji <jichanhong24@gmail.com>
2021-07-28 16:18:37 +0900

    Update todo with local-storage__update

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,17 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+let todos = [];
+
 function onTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
   paintTodo(newTodo);
+  todos.push(newTodo);
+  saveTodos();
 }
 
 function paintTodo(newTodo) {
@@ -26,4 +32,16 @@ function deleteTodo(event) {
   li.remove();
 }
 
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
 todoForm.addEventListener("submit", onTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
